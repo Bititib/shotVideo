77dd69420874b4c9792ee1dffda18f7f0b1a5c33
zhubo <admin@zhubo.asia>
2026-07-22 10:37:45 +0800

    fix: extend front-end validation to require at least one reference image if either audio or video references are uploaded

--- client/src/pages/analysis/VideoPage.tsx
+++ client/src/pages/analysis/VideoPage.tsx
@@ -814,8 +814,8 @@ export default function VideoPage() {
       setError('视频编辑模型必须上传参考视频');
       return;
     }
-    if (referenceAudios.length > 0 && referenceImages.length === 0) {
-      setError('参考音频模式下必须上传至少一张参考图');
+    if ((referenceAudios.length > 0 || referenceVideos.length > 0) && referenceImages.length === 0) {
+      setError('参考视频或音频模式下必须上传至少一张参考图');
       return;
     }
     setError(null);
